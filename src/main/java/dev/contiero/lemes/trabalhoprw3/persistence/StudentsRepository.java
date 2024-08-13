c5f946c0ddf95d54a9a5fc015c1929e80cb08381
gcontiero11 <gustcontiero@gmail.com>
2024-08-13 18:05:18 -0300

    feat: creating repository structure

--- src/main/java/dev/contiero/lemes/trabalhoprw3/persistence/StudentsRepository.java
+++ src/main/java/dev/contiero/lemes/trabalhoprw3/persistence/StudentsRepository.java
@@ -1,5 +1,24 @@
 package dev.contiero.lemes.trabalhoprw3.persistence;
 
+import dev.contiero.lemes.trabalhoprw3.domain.model.AlunoDTO;
+
+import java.util.Map;
+import java.util.Optional;
+
 public interface StudentsRepository {
-//    private  getStudents(){}
+    Map<AlunoDTO, Long> getAll();
+
+    Optional<AlunoDTO> getById();
+
+    Optional<AlunoDTO> getByRa();
+
+    Map<AlunoDTO, Long> getByName();
+
+    boolean save(AlunoDTO aluno);
+
+    boolean update(AlunoDTO aluno);
+
+    boolean delete(AlunoDTO aluno);
+
+
 }
