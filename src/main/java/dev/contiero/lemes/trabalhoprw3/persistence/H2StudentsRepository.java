6e35fab4e0f4d297684ce8ab81eae54c84d58bfc
gcontiero11 <gustcontiero@gmail.com>
2024-08-14 09:00:05 -0300

    feat: H2 Students Repo created

---
+++ src/main/java/dev/contiero/lemes/trabalhoprw3/persistence/H2StudentsRepository.java
@@ -0,0 +1,43 @@
+package dev.contiero.lemes.trabalhoprw3.persistence;
+
+import dev.contiero.lemes.trabalhoprw3.domain.model.AlunoDTO;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class H2StudentsRepository implements StudentsRepository{
+    @Override
+    public Map<AlunoDTO, Long> getAll() {
+        return Map.of();
+    }
+
+    @Override
+    public Optional<AlunoDTO> getById() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AlunoDTO> getByRa() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Map<AlunoDTO, Long> getByName() {
+        return Map.of();
+    }
+
+    @Override
+    public boolean save(AlunoDTO aluno) {
+        return false;
+    }
+
+    @Override
+    public boolean update(AlunoDTO aluno) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(AlunoDTO aluno) {
+        return false;
+    }
+}
